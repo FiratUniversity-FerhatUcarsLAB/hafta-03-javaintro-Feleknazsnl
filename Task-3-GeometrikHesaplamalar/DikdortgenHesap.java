6bd40b86cf95f459f604bb32c5a7d693e370471f
Feleknazsnl <240541039@firat.edu.tr>
2025-10-23 11:49:55 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.printf("Dikdörtgenin Alanı: %.2f\n", alan);
+        System.out.printf("Dikdörtgenin Çevresi: %.2f\n", cevre);
+    }
+}
