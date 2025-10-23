361a6f9a5e04b1648d67da85fca325e75e2c4c6b
Feleknazsnl <240541039@firat.edu.tr>
2025-10-23 11:51:51 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,13 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ahmet";
+        String soyad = "Yılmaz";
+        String ogrenciNo = "123456";
+        String bolum = "Bilgisayar Mühendisliği";
+
+        System.out.println("+-----------------------------------+");
+        System.out.println("|           ÖĞRENCİ KARTI           |");
+        System.out.println("+-----------------------------------+");
+        System.out.printf("| Ad         : %-20s |\n", ad);
+        System.out.printf("| Soyad      : %-20s |\n", soyad);
+        System.out.printf("| Öğrenci No : %-20s |\n", ogrenciNo);
