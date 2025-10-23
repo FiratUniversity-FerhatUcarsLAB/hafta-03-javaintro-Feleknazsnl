a5d012f809a21f2ca20d2935d49b1d95cb8fc386
Feleknazsnl <240541039@firat.edu.tr>
2025-10-23 12:01:39 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class ASCIISanat {
+    public static void main(String[] args) {
+        System.out.println("EEEEEEE");
+        System.out.println("E      ");
+        System.out.println("EEEE   ");
+        System.out.println("E      ");
+        System.out.println("EEEEEEE");
+    }
+}
