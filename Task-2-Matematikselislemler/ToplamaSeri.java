d855cc8bad857d2df010c00490d109edf0098b34
Feleknazsnl <240541039@firat.edu.tr>
2025-10-23 11:47:43 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        int toplam = 0;
+        for (int i = 1; i <= 9; i++) {
+            toplam += i;
+        }
+        System.out.println("1'den 9'a kadar sayıların toplamı: " + toplam);
+    }
+}
